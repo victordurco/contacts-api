260fc2be762aba7686905b6b18ed0bc8c55e96c5
victordurco <victordurco@gmail.com>
2022-02-27 00:50:06 -0300

    added email existence verification

--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -1,7 +1,7 @@
 import { getRepository, getManager } from 'typeorm';
 
 import ContactEntity from '../entities/ContactEntity';
-import { newContactSchema } from '../validations/contactSchema';
+import { emailAlreadyExists } from '../errors/contactErrors';
 
 import { Contact } from '../protocols/Contact';
 
@@ -11,7 +11,14 @@ export async function getAll(): Promise<Contact[]> {
     return contacts;
 }
 
+export async function getContactByEmail(email: string): Promise<Contact[]> {
+    const result = await getRepository(ContactEntity).find({email: email});
+    return result; 
+  }
+
 export async function createContact(contact: Contact): Promise<Contact> {
+    const usersWithThisEmail: Contact[] = await getContactByEmail(contact.email);
+    if (usersWithThisEmail.length) throw new emailAlreadyExists();
 
     const newContact = await getRepository(ContactEntity).create(contact);
     await getRepository(ContactEntity).save(newContact);
